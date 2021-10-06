d2567382366a25e487da02f37cc5c3f629d6fa92
Oduwole Oladipupo <42739211+MrDipo@users.noreply.github.com>
2021-10-06 22:23:04 +0100

    fixed spelling errors in comments

--- CellDistance/cell.py
+++ CellDistance/cell.py
@@ -1,8 +1,8 @@
 # ***CELL.PY***
 
 # checks if two cells are white
-# if they are, sumOfCoordinates mod 2 should be 0
-# returns 1 if both are even, returns 0 otherwise
+# if they are, sumOfCorrespondingCoordinates mod 2 should be 0
+# function returns 1 if both are white, returns 0 otherwise
 def isWhite(x1, y1, x2, y2): 
   isCellOneWhite = (x1+y1) % 2
   isCellTwoWhite = (x2+y2) % 2
@@ -10,13 +10,11 @@ def isWhite(x1, y1, x2, y2):
 
   return int(areBothWhite == 0)
   
-# uses slope of a straight of 2 cells to determine diagonal
-# slope of diagonal points on 45 degree line wrt to origin is always 1(change in y coordinate/change in X coordinate is constant)
+# uses slope of a straight line formula to determine if 2 cells are on the same diagonal
 def isDiagonal(x1, y1, x2, y2): 
     return int((y2 - y1)/(x2 -x1) == 1)
 
-# takes coordinates of 2 cells
-# returns positive distance between them
+# returns positive distance between 2 cells
 # if cells are on a diagonal, distance is difference between any 2 corresponding coordinates |x1 - x2| or |y1 - y2|
 def getLength(y1, y2):
-  return  abs(y2-y1) # if I wanted to be thorough, I could have said (|x1 - x2| + |y1 - y2|)/2 but there is really no difference
+  return  abs(y2-y1)

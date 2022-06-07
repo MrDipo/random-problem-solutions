f36dd63b170788f85f73826c4d09e105ab3222f3
MrDipo <42739211+MrDipo@users.noreply.github.com>
2022-06-07 21:24:51 +0100

    updated a comment

--- practicePythonOrg/characterInput01.py
+++ practicePythonOrg/characterInput01.py
@@ -1,6 +1,6 @@
 # Ask user for name & age
 user_name = input("What is your name?\n:")
-
 user_age = int(input("How old are you?\n:"))
-#find out when they will turn a 100
+
+#find a way to make this generic (not explicit)
 print(f"Hi {user_name}, you will turn 100 years old by {2122 - user_age}")

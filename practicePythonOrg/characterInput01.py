2baa49a9cb5ae2d2bb922eaad609029e31cac158
MrDipo <42739211+MrDipo@users.noreply.github.com>
2022-06-07 21:22:04 +0100

    Problem solved

---
+++ practicePythonOrg/characterInput01.py
@@ -0,0 +1,6 @@
+# Ask user for name & age
+user_name = input("What is your name?\n:")
+
+user_age = int(input("How old are you?\n:"))
+#find out when they will turn a 100
+print(f"Hi {user_name}, you will turn 100 years old by {2122 - user_age}")

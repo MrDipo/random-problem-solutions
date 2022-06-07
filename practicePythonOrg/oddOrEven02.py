e0b93dd9ea410523ec1035d89adeb33234439cc8
MrDipo <42739211+MrDipo@users.noreply.github.com>
2022-06-07 21:37:28 +0100

    Problem solved, tweak anticipated Find a way to type check user input

---
+++ practicePythonOrg/oddOrEven02.py
@@ -0,0 +1,7 @@
+# find a way to make this typecheck user input
+num = int(input("Gimme a number please\n:"))
+
+if num%2 == 0:
+    print("Your number is even!")
+else:
+    print("Your number is odd.")

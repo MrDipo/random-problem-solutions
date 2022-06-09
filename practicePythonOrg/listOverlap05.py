ab75415d13d60980ec157bc07b511410bdd0f5f3
MrDipo <42739211+MrDipo@users.noreply.github.com>
2022-06-09 22:14:04 +0100

    Problem solved extras to be done

---
+++ practicePythonOrg/listOverlap05.py
@@ -0,0 +1,13 @@
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+# Return list with elements in common
+a = set(a)
+b = set(b)
+c = a.intersection(b)
+print(list(c))
+
+# extras
+# randomly generate two lists to test
+
+# Write in one line

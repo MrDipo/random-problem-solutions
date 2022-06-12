2979809ed2f8d657fea23b40797ec4831b4ac94c
MrDipo <42739211+MrDipo@users.noreply.github.com>
2022-06-12 21:06:03 +0100

    game logic implemented

---
+++ practicePythonOrg/rockPaperScicssors08.py
@@ -0,0 +1,23 @@
+rock = "rock"
+paper = "paper"
+scissors = "scissors"
+winp1 = "Player 1 wins! Congratulations!"
+winp2 = "Player 2 wins! Congratulations!"
+
+p1 = input("Player 1, Rock-Paper-Scissors shoot!: ") # player1 move
+p2 = input("Player 2, Rock-Paper-Scissors shoot!: ") # player2 move
+p1 = p1.lower()
+p2 = p2.lower()
+
+if p1 == p2:
+    print("Draw!")
+elif p1 == rock and p2 == scissors:
+    print(winp1)
+elif p1 == scissors and p2 == paper:
+    print(winp1)
+elif p1 == paper and p2 == rock:
+    print(winp1)
+else:
+    print(winp2)
+
+

e284103a3a4025d003348d66f4f0fe3d3c53956c
MrDipo <42739211+MrDipo@users.noreply.github.com>
2022-06-17 20:44:15 +0100

    added type checking for user input

--- practicePythonOrg/guessingGameOne09.py
+++ practicePythonOrg/guessingGameOne09.py
@@ -10,12 +10,14 @@ print(test_num)
 print("I picked a random number between 1 and 9, guess what it is:")
 
 while user_guess != test_num:
-    try:
-        user_guess = int(input())
-    except ValueError:
-        print("Oops, that is not a valid number. Try again...")
+    while True:
+        try:
+            user_guess = int(input())
+            break
+        except ValueError:
+            print("Oops, that is not a valid number. Try again...")
     
-    if user_guess not in [range(1,10)]:
+    if user_guess not in range(1,10):
         print("Your guess is not between 1 and 9! Try again...")
     elif user_guess == test_num:
         print("You guessed correctly, cool.")

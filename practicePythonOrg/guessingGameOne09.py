d80bf54735fb54cca51faa8cf447f9864bfc7189
MrDipo <42739211+MrDipo@users.noreply.github.com>
2022-06-17 20:10:50 +0100

    added type checking and ability to guess again

--- practicePythonOrg/guessingGameOne09.py
+++ practicePythonOrg/guessingGameOne09.py
@@ -3,13 +3,23 @@ import random
 def getNum():
     return random.randint(1, 9)
 
-test_num = getNum()
-print(test_num)
-user_guess = int(input("I picked a random number between 1 and 9, guess what it is:\n"))
 
-if user_guess == test_num:
-    print("You guessed correctly, cool.")
-elif user_guess > test_num:
-    print("You guessed too high")
-elif user_guess < test_num:
-    print("You guessed too low")
+test_num = getNum()
+user_guess = -1
+print(test_num)
+print("I picked a random number between 1 and 9, guess what it is:")
+
+while user_guess != test_num:
+    try:
+        user_guess = int(input())
+    except ValueError:
+        print("Oops, that is not a valid number. Try again...")
+    
+    if user_guess not in [range(1,10)]:
+        print("Your guess is not between 1 and 9! Try again...")
+    elif user_guess == test_num:
+        print("You guessed correctly, cool.")
+    elif user_guess > test_num:
+        print("You guessed too high, try again...")
+    elif user_guess < test_num:
+        print("You guessed too low, try again...")

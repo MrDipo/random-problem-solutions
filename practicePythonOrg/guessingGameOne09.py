206e1c4d4d634de8e9e57f3615240eade2881a59
MrDipo <42739211+MrDipo@users.noreply.github.com>
2022-06-18 22:17:13 +0100

    Problem solved add ability to exit; add guess tracking

--- practicePythonOrg/guessingGameOne09.py
+++ practicePythonOrg/guessingGameOne09.py
@@ -3,29 +3,27 @@ import random
 def getNum():
     return random.randint(1, 9)
 
-play = 1
+no_of_guesses = 0 # extra
 test_num = getNum()
 print(test_num) # printing random value for easy debugging
-print("I picked a random number between 1 and 9, guess what it is:")
 
-while play:
-    while True: # this while block executes until the guess is correct, and validates input
-        try:
-            user_guess = int(input())
-            if user_guess not in range(1,10):
-                print("Your guess is not between 1 and 9! Try again...")
-            elif user_guess == test_num:
-                print("You guessed correctly, cool.")
-                break
-            elif user_guess > test_num:
-                print("You guessed too high, try again...")
-            elif user_guess < test_num:
-                print("You guessed too low, try again...")
-        except ValueError:
-            print("Oops, that is not a valid number. Try again...")
-
-    play = int(input("Enter \'0\' to quit OR enter \'1\' to play again:\n"))
-    test_num = getNum()
-    print(test_num)
-    print("I picked a new number, guess again:")
-    
+while True:
+    print("I picked a random number between 1 and 9, guess what it is:\n(Enter 'exit' to end game)")
+    try:
+        user_guess = input()
+        if user_guess.lower() == 'exit': # extra
+            break
+        elif int(user_guess) not in range(1,10):
+            print("Your guess is not between 1 and 9! Try again...")
+        elif int(user_guess) == test_num:
+            no_of_guesses += 1
+            print(f"You guessed correctly in {no_of_guesses} tries, cool.")
+            break
+        elif int(user_guess) > test_num:
+            no_of_guesses += 1
+            print("You guessed too high, try again...")
+        elif int(user_guess) < test_num:
+            no_of_guesses += 1
+            print("You guessed too low, try again...")
+    except ValueError:
+        print("Oops, that is not a valid number. Try again...")

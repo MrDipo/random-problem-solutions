1bf54868f4933a5915fd331db311fb0e084d9528
MrDipo <42739211+MrDipo@users.noreply.github.com>
2022-07-22 21:08:09 +0100

    Problem solved
    extra still needs to be solved. worth doing

--- practicePythonOrg/passwordGenerator16.py
+++ practicePythonOrg/passwordGenerator16.py
@@ -1,10 +1,16 @@
-import random
 import secrets
 import string
 
 def strong_passkey(length:int):
-    key = random.choice(string.ascii_letters+string.digits+string.punctuation, length)
+    # Using choice method from secrets to cryptographically generate a secure password
+    key = ''.join(secrets.choice(string.ascii_letters+string.digits+string.punctuation) for _ in range(length))
     return key
 
 
-print(strong_passkey(8))
+def main():
+    size = int(input("Please enter desired password length: "))
+    print(f"Your secure password is:{strong_passkey(size)}")
+
+
+if __name__ == "__main__":
+    main()

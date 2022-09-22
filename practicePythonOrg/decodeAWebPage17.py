486eccfed1e77f8cadcbfa7c4498fa2c04509512
MrDipo <42739211+MrDipo@users.noreply.github.com>
2022-09-22 20:54:33 +0100

    created solution class, get_all_titles method

--- practicePythonOrg/decodeAWebPage17.py
+++ practicePythonOrg/decodeAWebPage17.py
@@ -1,1 +1,16 @@
-# omo
+from bs4 import BeautifulSoup
+import requests
+
+
+class DecodeNewsPage:
+    def get_all_titles (url):
+        r = requests.get(url)
+        r_html = r.text
+
+        soup = BeautifulSoup(r_html)
+        titles = soup.find_all('h3')
+        return titles
+
+
+url = 'https://www.nytimes.com/'
+print(DecodeNewsPage.get_all_titles(url))
